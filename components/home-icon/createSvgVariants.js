aa7b2b2daee6fb7035675a1f4d90de4dd10390da
Scott Joseph <info@scottpauljoseph.com>
2025-07-31 11:56:42 +0100

    update

--- components/home-icon/createSvgVariants.js
+++ components/home-icon/createSvgVariants.js
@@ -3,16 +3,12 @@ export const createSvgVariants = (isTypefacesPage, isTypefacePath) => ({
   initial: {
     scale: 1,
     filter: `drop-shadow(0 0 8px rgba(255, 255, 255, 0.6))`,
-    color: isTypefacePath
-      ? "var(--text-primary)"
-      : "rgb(16, 12, 8)",
+    color: isTypefacesPage ? "rgb(16, 12, 8)" : "white",
   },
   hover: {
     scale: 1,
     filter: "drop-shadow(0 0 12px rgba(255, 255, 255, 0.8))",
-    color: isTypefacePath
-      ? "#006efe"
-      : "#006efe",
+    color: "#39ff14",
     transition: { duration: 0.2 },
   },
 });
